4db3df2b73f1fed49cd6e77430e67164ef7c1543
amongmany <danny@amongmany.com>
2015-04-07 14:57:50 +0000

    Implemented factorygirl gem for testing, turned off fixtures in test_helper

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class QuoteTest < ActiveSupport::TestCase
 
 	test "unique_tag" do
-		quote = Quote.create(:author => 'Danny Nathan', :saying => 'You only yolo once. Then you realize how stupid you look for using "yolo."')
+		quote = FactoryGirl.create(:quote, :author => 'Danny Nathan')
 		expected = 'DN#' + quote.id.to_s
 		actual = quote.unique_tag
 		assert_equal expected, actual
